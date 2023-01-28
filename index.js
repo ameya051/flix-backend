25269cccf17c888b9018b20ea8515faffebe0eef
Ameya Shrivastava <ameyshri051@gmail.com>
2023-01-29 03:23:43 +0530

    server

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/api/v1", routes);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`server listening at http://localhost:${port}`);
+});
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Mongodb connected");
+    server.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log({ err });
+    process.exit(1);
+  });
