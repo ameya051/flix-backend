60dc275cdec6469c25e59a0b50211b78766a2c92
Ameya Shrivastava <ameyshri051@gmail.com>
2023-01-29 16:20:40 +0530

    tmdb

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/api/v1", routes);
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
