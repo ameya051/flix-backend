60a7111e1be207166d0dfa32962349af3d6106f1
Ameya Shrivastava <ameyshri051@gmail.com>
2023-02-22 13:28:47 +0530

    deployment

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require("express");
-const http = require("http");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const mongoose = require("mongoose");
-require("dotenv/config");
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import http from "http";
+import mongoose from "mongoose";
+import "dotenv/config";
+import routes from "./src/routes/index.js";
 
 const app = express();
 
@@ -12,7 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use("/api/v1", routes);
+
 const port = process.env.PORT || 5000;
+
 const server = http.createServer(app);
 
 mongoose
@@ -27,3 +31,5 @@ mongoose
     console.log({ err });
     process.exit(1);
   });
+
+//test
